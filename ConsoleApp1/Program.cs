4eec5589e72c38167cb04e5ce9c6f553ca8b4f96
govin <govin@GOVIND>
2025-11-14 09:29:50 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+float baseSalary = 50000.0F;
+float hra = 0.30F;
+float netSalary = (baseSalary + (baseSalary * hra));
+Console.WriteLine("Base Salary:" + baseSalary);
+Console.WriteLine("Net Salary:" + netSalary);
